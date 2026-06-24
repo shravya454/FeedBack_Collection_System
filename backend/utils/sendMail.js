945af5437ff11cd9f55a11506a1ebc488c14b3f6
ShravyaKV <shravyakv9@gmail.com>
2026-06-24 10:24:56 +0530

    Update nodemailer configuration

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
